9ca8b822a2eb1195025f976f743f350f8e43a442
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2022-01-25 17:34:28 +0000

    .

--- scripts/modal.js
+++ scripts/modal.js
@@ -3,6 +3,7 @@ const btnCloseModal = document.getElementById("close-modal");
 
 const btnsOpenModal = document.querySelectorAll(".openmodal");
 console.log(btnsOpenModal);
+modal.style.display = "none";
 
 btnCloseModal.addEventListener("click", () => {
 	modal.style.display = "none";
